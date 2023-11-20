490d836c04e12d5d54229447ae879d6893f90161
Huy Vuong <huy-vuong@users.noreply.github.com>
2023-11-20 02:15:08 -0800

    refactor: shorten widget code for _MyHomePageState

--- lib/ui/pages/my_home_page.dart
+++ lib/ui/pages/my_home_page.dart
@@ -37,10 +37,8 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          children: [
+            const Text('You have pushed the button this many times:'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
